763aa6ea03149ca68b44fa54466be563b56da789
Gary <loayzag91@hotmail.com>
2016-12-18 14:46:50 -0600

    JSON 404 message

--- flaskr.py
+++ flaskr.py
@@ -3,7 +3,7 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, jsonify
+     render_template, flash, jsonify, make_response
 
 # create our little application :)
 app = Flask(__name__)
@@ -134,5 +134,11 @@ def get_post(id):
     return jsonify({'posts': post})
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+
