c27d74324c6bb7afb6de2ab8c03aa1bc54132375
Gary <loayzag91@hotmail.com>
2016-12-21 20:49:40 -0600

    Testing suite completed for Flaskr, without api testing.

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -5,11 +5,27 @@ from app.models import db
 
 
 class FlaskrTestCase(unittest.TestCase):
+
     def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
+        self.tester = current_app.test_client()
         db.create_all()
+
+    def login(self, username, password):
+        return self.tester.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.tester.get('/logout', follow_redirects=True)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
     def test_app_exists(self):
         self.assertFalse(current_app is None, msg=None)
@@ -17,10 +33,29 @@ class FlaskrTestCase(unittest.TestCase):
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'], msg=None)
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
+    def test_db_is_created(self):
+        rv = self.tester.get('/')
+        assert b'No entries here so far' in rv.data
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert b'You were logged in' in rv.data
+        rv = self.logout()
+        assert b'You were logged out' in rv.data
+        rv = self.login('adminx', 'default')
+        assert b'Invalid username' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert b'Invalid password' in rv.data
+
+    def test_messages(self):
+        self.login('admin', 'default')
+        rv = self.tester.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here'
+        ), follow_redirects=True)
+        assert b'No entries here so far' not in rv.data
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'<strong>HTML</strong> allowed here' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
